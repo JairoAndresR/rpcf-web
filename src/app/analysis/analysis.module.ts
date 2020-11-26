a4fbbf1cb3160c50b78590a8797bbae5dde910da
JairoAndresR <jairoandresr08@gmail.com>
2020-11-25 20:16:57 -0500

    form for advanced search created

--- src/app/analysis/analysis.module.ts
+++ src/app/analysis/analysis.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AnalysisRoutingModule } from './analysis-routing.module';
 import { HomeComponent } from './components/home/home.component';
+import { SearchFormComponent } from './components/search-form/search-form.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, SearchFormComponent],
   imports: [
     CommonModule,
     AnalysisRoutingModule
