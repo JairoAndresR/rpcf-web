12607ee766d1e9d3de48045af492ae07e70aee39
David Aparicio <davidalejandro1223@hotmail.com>
2020-11-30 11:24:54 -0500

    UI implementation for products CRUD

--- src/app/admin/components/products-form/products-form.component.ts
+++ src/app/admin/components/products-form/products-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
+import { Router } from '@angular/router';
+import { ProductService } from 'src/app/core/services/product/product.service';
 
 @Component({
   selector: 'app-products-form',
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsFormComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private productService: ProductService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.buildForm()
+  }
+
+  private buildForm(){
+    this.form = this.formBuilder.group({
+      'name': ['', Validators.required],
+      'definition': ['', Validators.required]
+    })
+  }
+
+  registerProduct(event:Event){
+    event.preventDefault();
+    if(this.form.valid){
+      this.productService.newProduct(this.form.value)
+      this.router.navigate(['/admin/collector-configuration/products'])
+    }
   }
 
 }
