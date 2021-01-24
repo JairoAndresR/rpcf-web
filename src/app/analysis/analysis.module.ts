36f2ee45e2e2f8e3e8de8e346265424b3ad65cbf
cristianchaparroa <cristianchaparroa@gmail.com>
2021-01-24 14:24:09 -0500

    fix: rename the directories and services

--- src/app/analysis/analysis.module.ts
+++ src/app/analysis/analysis.module.ts
@@ -10,8 +10,8 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatNativeDateModule} from '@angular/material/core'; 
 import { MatDatepickerModule } from "@angular/material/datepicker";
 import {MatButtonModule} from '@angular/material/button';
-import { BarchartAuthorsAndProductsComponent } from './components/barchart-authors-and-products/barchart-authors-and-products.component';
-import { BarchartGroupAndProductsComponent } from './components/barchart-group-and-products/barchart-group-and-products/barchart-group-and-products.component';
+import { BarchartAuthorsAndProductsComponent } from './components/barchart/barchart-authors-and-products/barchart-authors-and-products.component';
+import { BarchartGroupAndProductsComponent } from './components/barchart/barchart-group-and-products/barchart-group-and-products.component';
 import { ProductResultComponent } from './components/products-result/product-result.component';
 import {FormsModule} from '@angular/forms';
 
