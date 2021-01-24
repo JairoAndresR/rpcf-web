97ccee05df01b6e28f22604fd43d5764ac1951f5
cristianchaparroa <cristianchaparroa@gmail.com>
2021-01-24 12:23:13 -0500

    fix: change the double quoto to single quote

--- src/app/analysis/components/barchart-groupvsproducts/barchart-groupvsproducts/barchart-groupvsproducts.component.ts
+++ src/app/analysis/components/barchart-groupvsproducts/barchart-groupvsproducts/barchart-groupvsproducts.component.ts
@@ -7,15 +7,11 @@ import * as d3 from 'd3'
   styleUrls: ['./barchart-groupvsproducts.component.css']
 })
 export class BarchartGroupvsproductsComponent implements OnInit {
-  @Input() gruplacProductsQueryList
-  
-
+  @Input() gruplacProductsQueryList;
   private svg;
   private margin = 40;
   private width = 300 - (this.margin * 2);
   private height = 200 - (this.margin * 3);
-  
-  constructor() { }
 
   ngOnInit(): void {
     this.createSvg();
@@ -23,12 +19,12 @@ export class BarchartGroupvsproductsComponent implements OnInit {
   }
 
   private createSvg(): void {
-    this.svg = d3.select("figure#bargroup")
-    .append("svg")
-    .attr("width", this.width + (this.margin * 2))
-    .attr("height", this.height + (this.margin * 7.4))
-    .append("g")
-    .attr("transform", "translate(" + this.margin + "," + this.margin + ")");
+    this.svg = d3.select('figure#bargroup')
+    .append('svg')
+    .attr('width', this.width + (this.margin * 2))
+    .attr('height', this.height + (this.margin * 7.4))
+    .append('g')
+    .attr('transform', 'translate(' + this.margin + ',' + this.margin + ')');
   }
 
   private drawBars(gruplacProductsQueryList: any[]): void {
@@ -39,12 +35,12 @@ export class BarchartGroupvsproductsComponent implements OnInit {
     .padding(0.2);
 
     // Draw the X-axis on the DOM
-    this.svg.append("g")
-    .attr("transform", "translate(0," + this.height + ")")
+    this.svg.append('g')
+    .attr('transform', 'translate(0,' + this.height + ')')
     .call(d3.axisBottom(x))
-    .selectAll("text")
-    .attr("transform", "translate(-10,0)rotate(-45)")
-    .style("text-anchor", "end");
+    .selectAll('text')
+    .attr('transform', 'translate(-10,0)rotate(-45)')
+    .style('text-anchor', 'end');
 
     // Create the Y-axis band scale
     const y = d3.scaleLinear()
@@ -52,19 +48,19 @@ export class BarchartGroupvsproductsComponent implements OnInit {
     .range([this.height, 0]);
 
     // Draw the Y-axis on the DOM
-    this.svg.append("g")
+    this.svg.append('g')
     .call(d3.axisLeft(y));
 
     // Create and fill the bars
-    this.svg.selectAll("bars")
+    this.svg.selectAll('bars')
     .data(gruplacProductsQueryList)
     .enter()
-    .append("rect")
-    .attr("x", d => x(d.Researcher))
-    .attr("y", d => Number(y(d.Products)))
-    .attr("width", x.bandwidth())
-    .attr("height", (d) => this.height - Number(y(d.Products)))
-    .attr("fill", "#2E46D8");
+    .append('rect')
+    .attr('x', d => x(d.Researcher))
+    .attr('y', d => Number(y(d.Products)))
+    .attr('width', x.bandwidth())
+    .attr('height', (d) => this.height - Number(y(d.Products)))
+    .attr('fill', '#2E46D8');
   }
 
 }
