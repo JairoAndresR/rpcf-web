1fd9cd026b0e8b251841aa21343a2131a6338feb
JairoAndresR <jairoandresr08@gmail.com>
2021-01-29 10:58:06 -0500

    Paginator for advanced research results

--- src/app/analysis/components/products-result/product-result.component.ts
+++ src/app/analysis/components/products-result/product-result.component.ts
@@ -1,18 +1,39 @@
-import {Component, Input, OnChanges, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, ViewChild, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MatPaginator } from '@angular/material/paginator';
+import { Product } from '../../../core/models/product.model';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-results-advanced-search',
   templateUrl: './product-result.component.html',
   styleUrls: ['./product-result.component.css']
 })
-export class ProductResultComponent implements OnInit, OnChanges {
+
+export class ProductResultComponent implements OnInit, OnChanges, OnDestroy {
 
   @Input() products;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  obs: Observable<any>;
+  dataSource: MatTableDataSource<Product>;
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
 
   ngOnInit(): void {}
 
   ngOnChanges(): void {
-    console.log(this.products);
+    this.dataSource = new MatTableDataSource<Product>(this.products);
+    this.changeDetectorRef.detectChanges();
+    this.dataSource.paginator = this.paginator;
+    this.obs = this.dataSource.connect();
+  }
+
+  ngOnDestroy() {
+    if (this.dataSource) {
+      this.dataSource.disconnect();
+    }
   }
 
 }
