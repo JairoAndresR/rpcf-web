bb1cc5aa0c860dcafef318e2f9acabdf4cf9f768
Jairo Romero <jairo.romero@mercadolibre.com.co>
2023-04-11 22:56:01 -0500

    fix graph logic

--- src/app/analysis/components/barchart/barchart-group-and-products/barchart-group-and-products.component.ts
+++ src/app/analysis/components/barchart/barchart-group-and-products/barchart-group-and-products.component.ts
@@ -22,6 +22,9 @@ export class BarchartGroupAndProductsComponent implements OnInit, OnChanges {
 
   ngOnChanges(): void {
     this.maxProducts = Math.max.apply(Math, this.reportList.map((o) => o.count));
+    if (this.reportList.length === 0) {
+      return;
+    }
     this.drawBars(this.reportList);
   }
 
@@ -38,19 +41,18 @@ export class BarchartGroupAndProductsComponent implements OnInit, OnChanges {
   private drawBars(reportList: any[]): void {
     // Elimina todas las barras antiguas
     this.svg.selectAll('rect').remove();
-
+    console.log("its ok 1");
     // Limpia el objeto SVG
     this.svg.selectAll('*').remove();
-    if (reportList.length === 0) {
-      return;
-    }
+    console.log("its ok 2");
+    console.log(reportList.length);
 
     // Create the X-axis band scale
     const xAxis = d3.scaleBand()
       .range([0, this.width])
       .domain(reportList.map(d => d.value))
       .padding(0.2);
-
+    console.log("its ok 3");
     // Draw the X-axis on the DOM
     this.svg.append('g')
       .attr('transform', 'translate(0,' + this.height + ')')
@@ -58,16 +60,16 @@ export class BarchartGroupAndProductsComponent implements OnInit, OnChanges {
       .selectAll('text')
       .attr('transform', 'translate(-10,0)rotate(-45)')
       .style('text-anchor', 'end');
-
+    console.log("its ok 4");
     // Create the Y-axis band scale
     const yAxis = d3.scaleLinear()
       .domain([0, this.maxProducts+(this.maxProducts * 0.1)])
       .range([this.height, 0]);
-
+    console.log("its ok 5");
     // Draw the Y-axis on the DOM
     this.svg.append('g')
       .call(d3.axisLeft(yAxis));
-
+    console.log("its ok 6");
     // Create and fill the bars
     this.svg.selectAll('bars')
       .data(reportList)
@@ -78,7 +80,7 @@ export class BarchartGroupAndProductsComponent implements OnInit, OnChanges {
       .attr('width', xAxis.bandwidth())
       .attr('height', (report) => this.height - yAxis(report.count))
       .attr('fill', '#2E46D8');
-
+    console.log("its ok 7");
     this.svg.selectAll('rect')
         .on('mouseover', (event, d) => {
           this.tooltip.transition().duration(200).style('opacity', 0.9);
@@ -89,6 +91,7 @@ export class BarchartGroupAndProductsComponent implements OnInit, OnChanges {
         .on('mouseout', (event, d) => {
           this.tooltip.transition().duration(500).style('opacity', 0);
         });
+    console.log("its ok 8");
   }
 
 }
